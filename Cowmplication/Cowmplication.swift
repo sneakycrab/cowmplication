2d343bf198d10f9fbf0eefe820c015a74abb7823
Sneaky Crab <git@sneakycrab.com>
2015-06-22 23:00:29 -0700

    first commit

---
+++ Cowmplication/Cowmplication.swift
@@ -0,0 +1,82 @@
+//
+//  Cowmplication.swift
+//  Cowmplication
+//
+//  Copyright © 2015 Sneaky Crab. All rights reserved.
+//
+
+import Foundation
+import ClockKit
+
+class Cowmplication: NSObject, CLKComplicationDataSource {
+    func getCurrentHealth() -> Int {
+        // This would be used to retrieve current Chairman Cow health data
+        // for display on the watch. For testing, this always returns a
+        // constant.
+        return 4
+    }
+    
+    func getNextRequestedUpdateDateWithHandler(handler: (NSDate?) -> Void) {
+        // Update hourly
+        handler(NSDate(timeIntervalSinceNow: 60*60))
+    }
+    
+    func getPlaceholderTemplateForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTemplate?) -> Void) {
+        var template: CLKComplicationTemplate? = nil
+        switch complication.family {
+        case .ModularSmall:
+            template = nil
+        case .ModularLarge:
+            template = nil
+        case .UtilitarianSmall:
+            template = nil
+        case .UtilitarianLarge:
+            template = nil
+        case .CircularSmall:
+            let modularTemplate = CLKComplicationTemplateCircularSmallRingText()
+            modularTemplate.textProvider = CLKSimpleTextProvider(text: "--")
+            modularTemplate.fillFraction = 0.7
+            modularTemplate.ringStyle = CLKComplicationRingStyle.Closed
+            template = modularTemplate
+        }
+        handler(template)
+    }
+    
+    func getPrivacyBehaviorForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationPrivacyBehavior) -> Void) {
+        handler(CLKComplicationPrivacyBehavior.ShowOnLockScreen)
+    }
+    
+    func getCurrentTimelineEntryForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTimelineEntry?) -> Void) {
+        if complication.family == .CircularSmall {
+            let template = CLKComplicationTemplateCircularSmallRingText()
+            template.textProvider = CLKSimpleTextProvider(text: "\(getCurrentHealth())")
+            template.fillFraction = Float(getCurrentHealth()) / 10.0
+            template.ringStyle = CLKComplicationRingStyle.Closed
+            
+            let timelineEntry = CLKComplicationTimelineEntry(date: NSDate(), complicationTemplate: template)
+            handler(timelineEntry)
+        } else {
+            handler(nil)
+        }
+    }
+    
+    func getTimelineEntriesForComplication(complication: CLKComplication, beforeDate date: NSDate, limit: Int, withHandler handler: ([CLKComplicationTimelineEntry]?) -> Void) {
+        handler(nil)
+    }
+    
+    func getTimelineEntriesForComplication(complication: CLKComplication, afterDate date: NSDate, limit: Int, withHandler handler: ([CLKComplicationTimelineEntry]?) -> Void) {
+        handler([])
+    }
+    
+    func getSupportedTimeTravelDirectionsForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTimeTravelDirections) -> Void) {
+        handler([CLKComplicationTimeTravelDirections.None])
+    }
+    
+    func getTimelineStartDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
+        handler(NSDate())
+    }
+    
+    func getTimelineEndDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
+        handler(NSDate())
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# cowmplication
+
+This is sample code provided to go along with the [watchOS 2 Complication Tutorial](http://www.sneakycrab.com/blog/2015/6/10/writing-your-own-watchkit-complications) located in the Sneaky Crab Developer Blog. 
